aca7a0de2a81d4411e847a2cd47235c030aa6a2f
najimsadat <saidnajimullahs@yahoo.com>
2021-12-21 16:39:44 +0100

    Cart-btn animation on each addition added

--- src/components/Layout/CartButton.js
+++ src/components/Layout/CartButton.js
@@ -1,18 +1,35 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from "./CartButton.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 const CartButton = (props) => {
+  const [btnHighlighted, setBtnHighlight] = useState(false);
   const cartCtx = useContext(CartContext);
-
   const numberOfCartItem = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${btnHighlighted ? classes.bump : ""}`;
+  const { items } = cartCtx;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnHighlight(true);
+
+    const timer = setTimeout(() => {
+      setBtnHighlight(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
-        <CartIcon />
+        <CartIcon ca />
       </span>
       <span>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItem}</span>
